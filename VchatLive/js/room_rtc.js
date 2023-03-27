3cdd21178f7d375b054edf53d6cfd2db802be8d5
Aayushi <ee200002001@iiti.ac.in>
2023-03-27 19:23:02 +0530

    JoinStream

--- VchatLive/js/room_rtc.js
+++ VchatLive/js/room_rtc.js
@@ -23,6 +23,24 @@ let remoteUsers = {}
 let joinRoomInit = async () => {
     client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
     await client.join(APP_ID, roomId, token, uid)
+
+    joinStream()
 }
 
+let joinStream = async () => {
+    localTracks= await AgoraRTC.createMicrophoneAndCameraTracks()
+    
+   
+    let player = `<div class="video__container" id="user-container-${uid}">
+                    <div class="video-player" id="user-${uid}"></div>
+                 </div>`
+console.log( document.getElementById('streams__container'))
+    document.getElementById('streams__container').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${uid}`)
+    
+}
+
+
 joinRoomInit()
+
