67ac391f02e5793874af5ae0cd61040fa7fc166f
Aayushi <ee200002001@iiti.ac.in>
2023-03-30 22:27:35 +0530

    Create Rooms

--- VchatLive/js/room_rtc.js
+++ VchatLive/js/room_rtc.js
@@ -17,6 +17,12 @@ if (!roomId) {
   roomId = "main";
 }
 
+
+let displayName = sessionStorage.getItem('display_name')
+if(!displayName){
+    window.location = 'lobby.html'
+}
+
 let localTracks = [];
 let remoteUsers = {};
 let localScreenTracks;
