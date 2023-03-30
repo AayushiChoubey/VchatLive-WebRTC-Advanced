23a5c2fec3eebc86ffbb1bc0e9e227b5c25ef388
Aayushi <ee200002001@iiti.ac.in>
2023-03-30 18:49:35 +0530

    setCameraQuality

--- VchatLive/js/room_rtc.js
+++ VchatLive/js/room_rtc.js
@@ -31,7 +31,10 @@ let joinRoomInit = async () => {
 }
 
 let joinStream = async () => {
-    localTracks= await AgoraRTC.createMicrophoneAndCameraTracks()
+    localTracks= await AgoraRTC.createMicrophoneAndCameraTracks({}, {encoderConfig:{
+        width:{min:640, ideal:1920, max:1920},
+        height:{min:480, ideal:1080, max:1080}
+    }})
     
    
     let player = `<div class="video__container" id="user-container-${uid}">
